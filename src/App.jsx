f34ae3fe889bfb7d262b9ccedfa55b080df2353c
gulsane <craftygulsane@gmail.com>
2024-05-05 15:56:28 +0530

    Added: data to the app component from json file and passing it to paginator as prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import PageNavigator from "./components/pageNavigator/PageNavigator";
-
 import Paginator from "./components/paginator/Paginator";
+import data from "./data.json";
 
 function App() {
 	return (
 		<>
 			<div>
-				<Paginator />
+				<Paginator items={data} itemsPerPage={10} />
 			</div>
 		</>
 	);
