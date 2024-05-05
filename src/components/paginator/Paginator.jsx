40ce920be6a4de2ea426054d7eb29a721894b80a
gulsane <craftygulsane@gmail.com>
2024-05-05 16:03:06 +0530

    Added: table which display items dinamically in Paginator component

--- src/components/paginator/Paginator.jsx
+++ src/components/paginator/Paginator.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import PageNavigator from "../pageNavigator/PageNavigator";
+import "./index.css";
 
 const Paginator = ({ items = [], itemsPerPage = 10 }) => {
 	const [selectedPageIndex, setSelectedPageIndex] = useState(1);
 	const pages = Math.ceil(items.length / itemsPerPage);
+
+	const showableItems = items.slice(
+		(selectedPageIndex - 1) * itemsPerPage,
+		selectedPageIndex * itemsPerPage
+	);
 
 	const handlePageIndex = (index) => {
 		setSelectedPageIndex(index);
@@ -17,7 +23,7 @@ const Paginator = ({ items = [], itemsPerPage = 10 }) => {
 				}
 				break;
 			case "next-page":
-				if (selectedPageIndex < pageNumber) {
+				if (selectedPageIndex < pages) {
 					setSelectedPageIndex((prePageIndex) => prePageIndex + 1);
 				}
 				break;
@@ -25,20 +31,58 @@ const Paginator = ({ items = [], itemsPerPage = 10 }) => {
 				setSelectedPageIndex(1);
 				break;
 			case "last-page":
-				setSelectedPageIndex(pageNumber);
+				setSelectedPageIndex(pages);
 				break;
 			default:
 				break;
 		}
 	};
+
 	return (
 		<>
-			<PageNavigator
-				pageNumber={pages}
-				selectedPageIndex={selectedPageIndex}
-				handlePageIndex={handlePageIndex}
-				handlePageNavigator={handlePageNavigator}
-			/>
+			<div className="paginator">
+				<table className="table">
+					<thead className="table-header">
+						<tr>
+							<th>
+								<input type="checkbox" />
+							</th>
+							<th>Name</th>
+							<th>Email</th>
+							<th>Role</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						{showableItems.map((item) => (
+							<tr key={item.id}>
+								<td>
+									<input type="checkbox" />
+								</td>
+								<td>{item.name}</td>
+								<td>{item.email}</td>
+								<td>{item.role}</td>
+								<td>
+									<button>
+										<i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+									</button>
+									<button>
+										<i className="fa fa-trash-o" aria-hidden="true"></i>
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+				<div className="container">
+					<PageNavigator
+						pages={pages}
+						selectedPageIndex={selectedPageIndex}
+						handlePageIndex={handlePageIndex}
+						handlePageNavigator={handlePageNavigator}
+					/>
+				</div>
+			</div>
 		</>
 	);
 };
