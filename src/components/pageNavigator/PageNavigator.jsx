ca943cbabf654416b6080296315e2997c2563bb5
gulsane <craftygulsane@gmail.com>
2024-05-05 13:46:06 +0530

    Modified: Lifted up state in PageNavigator to its parent Paginator

--- src/components/pageNavigator/PageNavigator.jsx
+++ src/components/pageNavigator/PageNavigator.jsx
@@ -2,44 +2,17 @@ import { useState } from "react";
 
 import "./index.css";
 
-const PageNavigator = ({ pageNumber }) => {
-	const [selectedPageIndex, setSelectedPageIndex] = useState(1);
-
+const PageNavigator = ({
+	pageNumber,
+	selectedPageIndex,
+	handlePageIndex,
+	handlePageNavigator,
+}) => {
 	const pageIndexes = [];
 
 	for (let index = 1; index <= pageNumber; index++) {
 		pageIndexes.push(index);
 	}
-
-	const handlePageIndex = (index) => {
-		setSelectedPageIndex(index);
-	};
-
-	const handlePageNavigator = (navigatorSpecifier) => {
-		switch (navigatorSpecifier) {
-			case "previous-page":
-				if (selectedPageIndex > 1) {
-					setSelectedPageIndex((prePageIndex) => prePageIndex - 1);
-				}
-				break;
-			case "next-page":
-				if (selectedPageIndex < pageNumber) {
-					setSelectedPageIndex((prePageIndex) => prePageIndex + 1);
-				}
-				break;
-			case "first-page":
-				setSelectedPageIndex(1);
-
-				break;
-			case "last-page":
-				setSelectedPageIndex(pageNumber);
-
-				break;
-
-			default:
-				break;
-		}
-	};
 
 	return (
 		<div className="container">
