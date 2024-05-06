de5b7fb19be97833e296bc5352ee6373f5fbabeb
gulsane <craftygulsane@gmail.com>
2024-05-06 14:52:43 +0530

    Added: Input component

---
+++ src/components/input/Input.jsx
@@ -0,0 +1,35 @@
+import { forwardRef, useState, useRef, useImperativeHandle } from "react";
+
+const Input = forwardRef(({ type, value, ...props }, ref) => {
+	const inputRef = useRef();
+
+	const [fieldValue, setFieldValue] = useState(value);
+
+	useImperativeHandle(ref, () => {
+		return {
+			value() {
+				return inputRef.current.value;
+			},
+			focus() {
+				inputRef.current.focus();
+			},
+		};
+	});
+
+	const handleChange = (event) => {
+		setFieldValue(event.target.value);
+	};
+
+	return (
+		<input
+			ref={inputRef}
+			type={type}
+			value={fieldValue}
+			{...props}
+			onChange={handleChange}
+			on
+		/>
+	);
+});
+
+export default Input;
