a28f0af008ebbda3d6047153e8436b4f45782cce
gulsane <craftygulsane@gmail.com>
2024-05-08 10:30:03 +0530

    Added: delete handler, handleDelete to delete a specific data

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,26 @@ function App() {
 	const [details, setDetails] = useState(data);
 
 	const handleSave = (detail) => {
-		console.log("saving", detail);
-
 		const index = details?.findIndex((element) => element.id === detail.id);
 		details[index] = detail;
 		setDetails(Array.from([...details]));
 	};
+
+	const handleDelete = (id) => {
+		setDetails((preDetails) =>
+			Array.from(preDetails.filter((detail) => detail.id !== id))
+		);
+	};
+
 	return (
 		<>
 			<div>
-				<Paginator items={details} itemsPerPage={10} handleSave={handleSave} />
+				<Paginator
+					items={details}
+					itemsPerPage={10}
+					handleSave={handleSave}
+					handleDelete={handleDelete}
+				/>
 			</div>
 		</>
 	);
