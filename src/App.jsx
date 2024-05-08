5867f11afbce9a6335bbe1b055ad18cc029b08db
gulsane <craftygulsane@gmail.com>
2024-05-08 10:12:04 +0530

    Created: handle Save changes to App component and passing it to Paginator as prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import Paginator from "./components/paginator/Paginator";
 import data from "./data.json";
 
 function App() {
+	const [details, setDetails] = useState(data);
+
+	const handleSave = (detail) => {
+		console.log("saving", detail);
+
+		const index = details?.findIndex((element) => element.id === detail.id);
+		details[index] = detail;
+		setDetails(Array.from([...details]));
+	};
 	return (
 		<>
 			<div>
-				<Paginator items={data} itemsPerPage={10} />
+				<Paginator items={details} itemsPerPage={10} handleSave={handleSave} />
 			</div>
 		</>
 	);
