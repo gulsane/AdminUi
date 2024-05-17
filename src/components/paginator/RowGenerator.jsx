1e7969822ad75be25c15ba427cfe618f0b436e45
gulsane <craftygulsane@gmail.com>
2024-05-17 09:52:28 +0530

    Modified: Paginator.jsx, moved RowGenerator into seperate file

---
+++ src/components/paginator/RowGenerator.jsx
@@ -0,0 +1,78 @@
+import { useRef } from "react";
+import Input from "../input/Input";
+
+export const RowGenerator = ({
+	rowDetails,
+	handleSave,
+	handleDelete,
+	handleEdit,
+	selected,
+	handleSelect,
+	editing,
+}) => {
+	const nameRef = useRef();
+	const emailRef = useRef();
+	const roleRef = useRef();
+
+	const onSave = () => {
+		const details = {
+			id: rowDetails.id,
+			name: nameRef.current.value(),
+			email: emailRef.current.value(),
+			role: roleRef.current.value(),
+		};
+		handleSave(details);
+	};
+
+	return (
+		<tr
+			className={`${selected ? "row-selected" : ""} ${editing ? "editing" : ""}`}
+		>
+			<td>
+				<input
+					type="checkbox"
+					checked={selected}
+					onChange={() => handleSelect(rowDetails.id)}
+				/>
+			</td>
+			<td>
+				<Input
+					type="text"
+					value={rowDetails.name}
+					ref={nameRef}
+					disabled={!editing}
+				/>
+			</td>
+			<td>
+				<Input
+					type="text"
+					value={rowDetails.email}
+					ref={emailRef}
+					disabled={!editing}
+				/>
+			</td>
+			<td>
+				<Input
+					type="text"
+					value={rowDetails.role}
+					ref={roleRef}
+					disabled={!editing}
+				/>
+			</td>
+			<td className="action-buttons">
+				{editing ? (
+					<button onClick={onSave} className="save">
+						<i className="fa fa-floppy-o" aria-hidden="true"></i>
+					</button>
+				) : (
+					<button onClick={() => handleEdit(rowDetails.id)} className="edit">
+						<i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+					</button>
+				)}
+				<button onClick={() => handleDelete(rowDetails.id)} className="delete">
+					<i className="fa fa-trash-o" aria-hidden="true"></i>
+				</button>
+			</td>
+		</tr>
+	);
+};
